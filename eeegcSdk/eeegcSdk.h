70b00339b4c51ee9dc9713f81153587c3a26cd6b
zhangbin <xmagic@betofly.cn>
2019-09-04 16:45:35 +0800

    build grpc tag v1.22.0 ok

---
+++ eeegcSdk/eeegcSdk.h
@@ -0,0 +1,19 @@
+//
+//  eeegcSdk.h
+//  eeegcSdk
+//
+//  Created by zhangbin on 2019/9/4.
+//  Copyright © 2019 zhangbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for eeegcSdk.
+FOUNDATION_EXPORT double eeegcSdkVersionNumber;
+
+//! Project version string for eeegcSdk.
+FOUNDATION_EXPORT const unsigned char eeegcSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <eeegcSdk/PublicHeader.h>
+
+
